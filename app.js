c801c4957b6a43979750b1851a8d839934aa5ccf
Elena <fed.len4ik@mail.ru>
2024-02-19 19:18:47 +0300

    2

--- app.js
+++ app.js
@@ -3,14 +3,4 @@ headerMenu.onclick = function(){
     console.log('work')
     let menu = document.getElementById('menu')
     menu.classList.toggle('display-none')
-
-    let info = document.getElementById('info')
-    info.classList.toggle('display-none')
-
-    let map = document.getElementById('map')
-    map.classList.toggle('display-none')
-
-
-    let phone = document.getElementById('phone')
-    phone.classList.toggle('display-none')
 }
