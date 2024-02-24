e02daa5c9668dd7a6866a2a064e6b17508547889
Elena <fed.len4ik@mail.ru>
2024-02-24 16:04:39 +0300

    5

--- app.js
+++ app.js
@@ -4,3 +4,35 @@ headerMenu.onclick = function(){
     let menu = document.getElementById('menu')
     menu.classList.toggle('display-none')
 }
+let info = document.getElementById('info')
+info.onclick = function(){
+    console.log('info')
+    let map = document.getElementById('map')
+    map.classList.toggle('display-none')
+    let phone = document.getElementById('phone')
+    phone.classList.toggle('display-none')
+    let info_img = document.getElementById('info-img')
+    info_img.classList.toggle('display-none')
+}
+
+let map = document.getElementById('map')
+map.onclick = function(){
+    console.log('map')
+    let info = document.getElementById('info')
+    info.classList.toggle('display-none')
+    let phone = document.getElementById('phone')
+    phone.classList.toggle('display-none')
+    let map_img = document.getElementById('map-img')
+    map_img.classList.toggle('display-none')
+}
+
+let phone = document.getElementById('phone')
+phone.onclick = function(){
+    console.log('phone')
+    let info = document.getElementById('info')
+    info.classList.toggle('display-none')
+    let map = document.getElementById('map')
+    map.classList.toggle('display-none')
+    let phone_img = document.getElementById('phone-img')
+    phone_img.classList.toggle('display-none')
+}
